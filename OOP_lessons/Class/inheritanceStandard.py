38927676a83de8470b4d9f8254b85d41c6224ceb
Nilton <niltonjuniorphd@gmail.com>
2024-07-09 13:31:48 -0300

    editing testRun.py file

--- OOP_lessons/Class/inheritanceStandard.py
+++ OOP_lessons/Class/inheritanceStandard.py
@@ -1,4 +1,3 @@
-from pyparsing import C
 from classWriteStandard import ClassName
 
 class Inheritance(ClassName):
@@ -11,8 +10,10 @@ class Inheritance(ClassName):
     '''
     def __init__(self, name_var1, name_var2, name_var3, name_var_new):
         ClassName.__init__(self, name_var1, name_var2, name_var3)
+        self.var_new = name_var_new
 
     def FunctionActionPrint2(self):
         print("This is a function that print the value of the var1 from class Inheritance") #print some text to the screen
         print(self.var1) #print the value of the variable var1
+        print(self.var_new) #print the value of the variable var_new
         
