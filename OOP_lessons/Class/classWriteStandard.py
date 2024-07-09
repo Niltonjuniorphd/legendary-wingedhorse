be2593a4ba92cfab4fa0a9f709c6eb6df6cb5b40
Nilton <niltonjuniorphd@gmail.com>
2024-07-09 09:55:56 -0300

    editing the code of classWriteStandard.py

--- OOP_lessons/Class/classWriteStandard.py
+++ OOP_lessons/Class/classWriteStandard.py
@@ -42,3 +42,21 @@ print(my_object.var1) #print the changed value of the variable var1
 my_object.FunctionActionPrint() #call the function FunctionActionPrint
 
 print(my_object.FunctionActionReturn(0.1)) #print the result of the function FunctionActionReturn with the value of the parameter name_param1
+
+#instantiating several objects of the class ClassName
+my_object_2 = ClassName('juice', 'C', 18)
+my_object_3 = ClassName('tea', 'H', 12)
+my_object_4 = ClassName('sprinkle_water', 'C', 15)
+
+#creating alist of the objects of the class ClassName
+my_list = []
+my_list.append(my_object)
+my_list.append(my_object_2)
+my_list.append(my_object_3)
+my_list.append(my_object_4)
+
+for i in my_list:
+    print(i.var1) #print the value of the variable var1 of each object of the list
+    print(i.var2) #print the value of the variable var2 of each object of the list
+    print(i.var3) #print the value of the variable var3 of each object of the list
+    print('\n') #print a new blank line
