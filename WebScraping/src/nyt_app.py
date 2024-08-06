ae10d37a3a6b6ebd8081160cc655686faf33e1f7
Nilton Oliveira <niltonjuniorphd@gmail.com>
2024-08-06 19:33:36 -0300

    create eviroment variable target for nyt_app

--- WebScraping/src/nyt_app.py
+++ WebScraping/src/nyt_app.py
@@ -1,30 +1,34 @@
-
-#%%
 import requests
 import pandas as pd
+import os
 
 
-full_text_nyt = ''
 
-df0 = pd.DataFrame()
-key_word = 'japão'
+def nyt_summary(key_word):
+    NYT_KEY = os.environ["NYT_KEY"]
+    full_text_nyt = ''
+    df0 = pd.DataFrame()
 
-try:
     for page in range(0, 5):
         response = []
         data = []
         artcles = []
-        url = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={key_word}&page={page}&api-key=XsoNAoGpQgAjeWQmXobmXYOYXJwRbh3Y'
+
+        url = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={key_word}&page={page}&api-key={NYT_KEY}'
+
         response = requests.get(url)
+
         data = response.json()
+
         articles = data['response']['docs']
+
         articles_df = pd.DataFrame(articles)
+
         df0 = pd.concat([df0, articles_df], axis=0)
 
     df0 = df0.reset_index(drop=True)
 
     print(df0.columns.to_list())
-
 
     full_text_nyt = ' '.join(df0['abstract'].unique())\
         .replace('...', '')\
@@ -38,11 +42,5 @@ try:
         .replace(',', '')\
         .replace('"', '')\
         .strip()
-    
-except KeyError as e:
-    print(f"KeyError: {e} - The key was not found in the searching.")
-except TypeError as e:
-    print(f"TypeError: {e} - The data structure is not in the expected format.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
-# %%
+
+    return full_text_nyt
